ada930286b18eb1d19e9d37f029253bf706125ed
jl_leonr <jl_leonr@yahoo.com>
2017-12-28 01:41:23 -0400

    Removed @Component annotation

--- Section4/spring-in-5-steps/src/main/java/com/springmasterclass/basics/springin5steps/BubbleSortAlgorithm.java
+++ Section4/spring-in-5-steps/src/main/java/com/springmasterclass/basics/springin5steps/BubbleSortAlgorithm.java
@@ -1,5 +1,8 @@
 package com.springmasterclass.basics.springin5steps;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class BubbleSortAlgorithm implements SortAlgorithm{
 	
 	public int[] sort(int[] numbers) {
