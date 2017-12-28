ac198824aea3e666937cbafa3643dd69253aa510
jl_leonr <jl_leonr@yahoo.com>
2017-12-28 02:33:21 -0400

    Copied files from previous section.

---
+++ Section5/spring-in-depth/spring-in-depth/src/main/java/com/springmasterclass/basics/springin5steps/springindepth/QuickSortAlgorithm.java
@@ -0,0 +1,17 @@
+package com.springmasterclass.basics.springin5steps.springindepth;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary
+public class QuickSortAlgorithm implements SortAlgorithm{
+
+	public int[] sort(int[] numbers) {
+
+		// logic for Quick Sort
+
+		return numbers;
+	}
+
+}
---
+++ Section5/spring-in-depth/spring-in-depth/src/main/java/com/springmasterclass/basics/springin5steps/springindepth/SpringInDepthApplication.java
@@ -0,0 +1,33 @@
+package com.springmasterclass.basics.springin5steps.springindepth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class SpringInDepthApplication {
+	
+	//What are the beans?
+		//Beans are objects that will be managed by the Spring framework,
+		//they are identified with the @Component annotation.
+	
+	//What are the dependencies of a bean?
+		//Dependencies are managed using the @Autowired annotation.
+	
+	//Where to search for the beans?
+		//Spring looks for beans within the package where the 
+		//@SpringBootApplication annotation is present.
+
+	public static void main(String[] args) {
+		//BinarySearchImpl binarySearch = new BinarySearchImpl(new BubbleSortAlgorithm());
+		//int result = binarySearch.binarySearch(new int[] { 12, 4, 6 }, 3);
+		
+		ConfigurableApplicationContext applicationContext = 
+				SpringApplication.run(SpringInDepthApplication.class, args);
+		
+		BinarySearchImpl binarySearch = applicationContext.getBean(BinarySearchImpl.class);
+		
+		int result = binarySearch.binarySearch(new int[] { 12, 4, 6 }, 3);
+		System.out.println(result);
+	}
+}
