ac353e8cc76f71624120ceff5bd7e65e6acdfbb6
jl_leonr <jl_leonr@yahoo.com>
2018-01-01 21:07:54 -0400

    Fixed directory structure.

---
+++ Section5/spring-in-depth/src/main/java/com/springmasterclass/basics/springin5steps/springindepth/BubbleSortAlgorithm.java
@@ -0,0 +1,14 @@
+package com.springmasterclass.basics.springin5steps.springindepth;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class BubbleSortAlgorithm implements SortAlgorithm{
+	
+	public int[] sort(int[] numbers) {
+		
+		//logic for Bubble Sort
+		
+		return numbers;
+	}
+}
---
+++ Section5/spring-in-depth/src/test/java/com/springmasterclass/basics/springin5steps/springindepth/SpringInDepthApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springmasterclass.basics.springin5steps.springindepth;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringInDepthApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
