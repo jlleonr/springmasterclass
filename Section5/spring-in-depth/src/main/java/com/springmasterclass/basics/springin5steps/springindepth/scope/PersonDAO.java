a679c6c2f9dfe2443473d565e06cd1a7963e6aac
jl_leonr <jl_leonr@yahoo.com>
2018-01-18 21:50:52 -0600

    Added PostConstruct/PreDestroy annotations.

--- Section5/spring-in-depth/src/main/java/com/springmasterclass/basics/springin5steps/springindepth/scope/PersonDAO.java
+++ Section5/spring-in-depth/src/main/java/com/springmasterclass/basics/springin5steps/springindepth/scope/PersonDAO.java
@@ -1,5 +1,10 @@
 package com.springmasterclass.basics.springin5steps.springindepth.scope;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
@@ -8,6 +13,8 @@ import org.springframework.stereotype.Component;
 @Component
 @Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
 public class PersonDAO {
+	
+	Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	@Autowired
 	JdbcConnection jdbcConnection;
@@ -18,5 +25,16 @@ public class PersonDAO {
 
 	public void setJdbcConnection(JdbcConnection jdbcConnection) {
 		this.jdbcConnection = jdbcConnection;
+	}
+	
+	@PostConstruct
+	public void postConstruct() {
+		logger.info("postConstruct");
+
+	}
+	
+	@PreDestroy
+	public void preDestroy() {
+		logger.info("preDestroy");
 	}
 }
