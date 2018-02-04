5baef239bb07d84449f9413c026c81514a550730
jl_leonr <jl_leonr@yahoo.com>
2018-02-04 15:36:06 -0600

    Changed @Component to @Service

--- Section5/spring-in-depth/src/main/java/com/springmasterclass/basics/springin5steps/springindepth/basic/BubbleSortAlgorithm.java
+++ Section5/spring-in-depth/src/main/java/com/springmasterclass/basics/springin5steps/springindepth/basic/BubbleSortAlgorithm.java
@@ -1,8 +1,8 @@
 package com.springmasterclass.basics.springin5steps.springindepth.basic;
 
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
+@Service
 //@Primary
 public class BubbleSortAlgorithm implements SortAlgorithm{
 	
