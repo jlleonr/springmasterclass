482a340475de1ea3d5b965ce8095aff6d3c9da33
jl_leonr <jl_leonr@yahoo.com>
2018-03-04 17:01:19 -0600

    Files added to create a SpringBoot simple MVC controller

---
+++ Section9/springboot-in-10steps/src/main/java/com/in28steps/springboot/basics/springbootin10steps/SpringbootIn10stepsApplication.java
@@ -0,0 +1,12 @@
+package com.in28steps.springboot.basics.springbootin10steps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootIn10stepsApplication {
+
+	public static void main(String[] args) {
+		 SpringApplication.run(SpringbootIn10stepsApplication.class, args);
+	}
+}
