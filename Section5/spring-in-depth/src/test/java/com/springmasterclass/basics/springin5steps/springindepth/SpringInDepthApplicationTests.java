248cb34497cae5491e2835f356061df85abb6457
jl_leonr <jl_leonr@yahoo.com>
2018-03-04 16:55:57 -0600

    Commented SPringBoot test to use JUnit

--- Section5/spring-in-depth/src/test/java/com/springmasterclass/basics/springin5steps/springindepth/SpringInDepthApplicationTests.java
+++ Section5/spring-in-depth/src/test/java/com/springmasterclass/basics/springin5steps/springindepth/SpringInDepthApplicationTests.java
@@ -2,11 +2,11 @@ package com.springmasterclass.basics.springin5steps.springindepth;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+//@SpringBootTest
 public class SpringInDepthApplicationTests {
 
 	@Test
